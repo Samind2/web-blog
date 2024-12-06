e4e02ce143c6c705e6875b6632467cb420ccf930
Samind2 <suphaneerangsirat@gmail.com>
2024-12-06 17:01:54 +0700

    Login

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <div>
-      <label className="input input-bordered flex items-center gap-2">
+      <label className="input input-bordered flex items-center gap-6 mt-4 mb-4">
         <input type="text" className="grow" placeholder="Username" />
       </label>
       <label className="input input-bordered flex items-center gap-2">
